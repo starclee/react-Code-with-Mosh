39f085760e5fb4d99aa86a402d475f0f0512274b
Sabari <sabarivijay214@gmail.com>
2023-11-28 00:26:27 +0530

    learned passing func to the props, type annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import ListGroup from "./components/ListGroup.tsx";
 
 function App() {
+  const items = ["Starc", "Steve", "Green", "Warner", "Head"];
+
+  const handleSelectEvent = (item: string) => {
+    console.log("Selected item: " + item);
+  };
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        title="Players"
+        onSelectedItem={handleSelectEvent}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,33 @@
-function ListGroup() {
-  const items = ["Starc", "Steve", "Green", "Warner", "Head"];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  title: string;
+  onSelectedItem: (item: string) => void;
+}
+
+function ListGroup({ items, title, onSelectedItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{title} List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, i) => {
           return (
-            <li key={i} className="list-group-item">
+            <li
+              key={i}
+              className={
+                selectedIndex === i
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              onClick={() => {
+                setSelectedIndex(i);
+                onSelectedItem(item);
+              }}
+            >
               {item}
             </li>
           );
