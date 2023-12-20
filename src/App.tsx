0b8cde208d3da32535abb8de8c904fbc9a05509e
Sabari <sabarivijay214@gmail.com>
2023-12-20 22:58:46 +0530

    css lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-// import ListGroup from "./components/ListGroup.tsx";
-
-// function App() {
-//   const items = ["Starc", "Steve", "Green", "Warner", "Head"];
-
-//   const handleSelectEvent = (item: string) => {
-//     console.log("Selected item: " + item);
-//   };
-//   return (
-//     <div>
-//       <ListGroup
-//         items={items}
-//         title="Players"
-//         onSelectedItem={handleSelectEvent}
-//       />
-//     </div>
-//   );
-// }
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup.tsx";
 
 function App() {
-  const [visibility, setVisibility] = useState(false);
+  const items = ["Starc", "Steve", "Green", "Warner", "Head"];
 
+  const handleSelectEvent = (item: string) => {
+    console.log("Selected item: " + item);
+  };
   return (
     <div>
-      {visibility && (
-        <Alert onClose={() => setVisibility(false)}>
-          Hello <b>Starc</b>
-        </Alert>
-      )}
-      <Button color="info" onClick={() => setVisibility(true)}>
-        Click here...
-      </Button>
+      <ListGroup
+        items={items}
+        title="Players"
+        onSelectedItem={handleSelectEvent}
+      />
     </div>
   );
 }
+
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+
+// function App() {
+//   const [visibility, setVisibility] = useState(false);
+
+//   return (
+//     <div>
+//       {visibility && (
+//         <Alert onClose={() => setVisibility(false)}>
+//           Hello <b>Starc</b>
+//         </Alert>
+//       )}
+//       <Button color="info" onClick={() => setVisibility(true)}>
+//         Click here...
+//       </Button>
+//     </div>
+//   );
+// }
 
 // function App() {
 //   return (
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 interface Props {
   items: string[];
   title: string;
   onSelectedItem: (item: string) => void;
 }
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "teal" : "none")};
+`;
 
 function ListGroup({ items, title, onSelectedItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -13,26 +28,27 @@ function ListGroup({ items, title, onSelectedItem }: Props) {
     <>
       <h1>{title} List</h1>
       {items.length === 0 && <p>No items found</p>}
-      <ul className="list-group">
+      <List>
         {items.map((item, i) => {
           return (
-            <li
+            <ListItem
+              active={i === selectedIndex}
               key={i}
-              className={
-                selectedIndex === i
-                  ? "list-group-item active"
-                  : "list-group-item"
-              }
+              // className={
+              //   selectedIndex === i
+              //     ? "list-group-item active"
+              //     : "list-group-item"
+              // }
               onClick={() => {
                 setSelectedIndex(i);
                 onSelectedItem(item);
               }}
             >
               {item}
-            </li>
+            </ListItem>
           );
         })}
-      </ul>
+      </List>
     </>
   );
 }
